cadb749e70cfc0c9dce58b356cbd67c51a7bcd9a
Rich Quick <richard.quick@arnoldclark.co.uk>
2012-02-21 16:41:21 +0000

    First migration

--- db/migrate/20120221155443_create_users.rb
+++ db/migrate/20120221155443_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string "email", :default => "", :null => false
+      t.string "password_hash", :null => false
+      t.boolean "active"
       t.timestamps
     end
   end
