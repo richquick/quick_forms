72f5ced18e423f09f953b7ed031216719a7190fe
Rich Quick <richard.quick@arnoldclark.co.uk>
2012-02-21 17:25:44 +0000

    New Rails Project (again)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickForms::Application.config.secret_token = 'ec0be7ecce7059ed0cb6264fbff05adc0ebc05b33e04bf39238894721d9cfb5e4e44b88672f02dac4050563846b34c5f419b96a6c532d10dde36bd4447fd7302'
