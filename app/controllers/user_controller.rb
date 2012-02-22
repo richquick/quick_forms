1cbb5a67281d2872b6ace040bbd0e3a9daa8147a
Rich Quick <richard.quick@arnoldclark.co.uk>
2012-02-22 17:28:03 +0000

    working on getting the basic CRUD working for user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,20 @@ class UserController < ApplicationController
   end
   
   def new
+    @user = User.new
+  end
+  
+  def create
+    # Instantiate a new object using form parameters
+    @user = User.new(params[:subject])
+    # Save the object
+    if @user.save
+      # If save succeeds, redirect to the list action
+      redirect_to(:action => 'list')
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def list
@@ -13,11 +27,32 @@ class UserController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    # Find object using form parameters
+    @user = User.find(params[:id])
+    # Update the object
+    if @user.update_attributes(params[:subject])
+      # If update succeeds, redirect to the list action
+      redirect_to(:action => 'show', :id => @user.id)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('edit')
+    end
   end
 
   def delete
+    @user = User.find(params[:id])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to(:action => 'list')
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def status_tag(boolean, options={})
+    options[:true]        ||= 'true'
+    options[:true_class]  ||= 'true'
+    options[:false]       ||= 'false'
+    options[:false_class] ||= 'false'
+
+    if boolean
+      content_tag(:span, options[:true], :class => options[:true_class])
+    else
+      content_tag(:span, options[:false], :class => options[:false_class])
+    end
+  end
+  
+  def error_messages_for( object )
+    render(:partial => 'shared/error_messages', :locals => {:object => object})
+  end
 end
